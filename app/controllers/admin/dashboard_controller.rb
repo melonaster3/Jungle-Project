4ce8feea1c4d6e26fbda7243c6095c69569d72dd
melonaster3 <melonaster@gmail.com>
2022-06-29 17:56:54 -0600

    Admin Dashboard count added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s
 
   def show
+ 
+      @product_count = Product.sum(:quantity)
+      @category_count = Product.count(:id)
+
   end
 end
